2c9d22e878346dd2cc12470fc71073b8e9cfe958
Andson Barbosa da Silva <54246685+andsonbarbosa@users.noreply.github.com>
2021-09-06 09:42:00 -0300

    Add files via upload

---
+++ AgenciaBancaria.Dominio/ContaCorrente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class ContaCorrente : ContaBancaria
+    {
+        public ContaCorrente(Cliente cliente, decimal limite): base(cliente)
+        {
+            ValorTaxaManutencao = 0.05M;
+            Limite = limite;
+        }
+
+        public override void Secar(decimal valor, string senha)
+        {
+            if (Senha != senha)
+            {
+                throw new Exception("Senha inválida.");
+            }
+
+            if ((Saldo + Limite) < valor)
+            {
+                throw new Exception("saldo insuficiente.");
+            }
+        }
+
+        public decimal Limite { get; private set; }
+
+        public decimal ValorTaxaManutencao { get; private set; }
+    }
+}
